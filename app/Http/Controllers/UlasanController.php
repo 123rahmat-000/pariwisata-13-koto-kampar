8510b0c6f6e8056a3a8765f9b59ee886d64b3f9b
Rahmad Hidayat <rahmat.hidayad1811@gmail.com>
2026-08-11 15:09:54 +0700

    menambah folder baru di atraksi, upload gambar atraksi

---
+++ app/Http/Controllers/UlasanController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth; 
+use Illuminate\Http\Request;
+use App\Models\Destinasi;
+use App\Models\Ulasan;
+use App\Models\User;
+
+class UlasanController extends Controller
+{
+    //
+    public function create($destinasiId)
+{
+    $destinasi = Destinasi::findOrFail($destinasiId);
+    return view('ulasan-create', compact('destinasi'));
+}
+
+
+ 
+public function store(Request $request)
+{
+    $validated = $request->validate([
+        'destinasi_id' => 'required|exists:destinasi,id',
+        'rating' => 'required|integer|min:1|max:5',
+        'komentar' => 'required|min:5',
+    ]);
+    $validated['user_id'] = Auth::id();
+ 
+    Ulasan::create($validated);
+ 
+    return redirect()->route('destinasi.detail', $validated['destinasi_id'])
+        ->with('success', 'Ulasan berhasil ditambahkan!');
+}
+
+}
+
+
+
